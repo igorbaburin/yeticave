0b9a8927800878e38320dfda39360d3413b5f67a
baburin.sinergium.ru <baburin@sinergium.ru>
2023-09-03 14:37:43 +0300

    02 ДЗ - выводит список товаров и категорий

--- index.php
+++ index.php
@@ -4,6 +4,46 @@ $is_auth = (bool) rand(0, 1);
 $user_name = 'Константин';
 $user_avatar = 'img/user.jpg';
 $category = ['Доски и лыжи', 'Крепления', 'Ботинки', 'Одежда', 'Инструменты', 'Разное'];
+$goods = [
+    0 => [
+        'name' => '2014 Rossignol District Snowboard',
+        'category' => 'Доски и лыжи',
+        'price' => '10999',
+        'img' => 'img/lot-1.jpg',
+    ],
+    1 => [
+        'name' => 'DC Ply Mens 2016/2017 Snowboard',
+        'category' => 'Доски и лыжи',
+        'price' => '159999',
+        'img' => 'img/lot-2.jpg',
+    ],
+    2 => [
+        'name' => 'Крепления Union Contact Pro 2015 года размер L/XL',
+        'category' => 'Крепления',
+        'price' => '8000',
+        'img' => 'img/lot-3.jpg',
+    ],
+    3 => [
+        'name' => 'Ботинки для сноуборда DC Mutiny Charocal',
+        'category' => 'Ботинки',
+        'price' => '10999',
+        'img' => 'img/lot-4.jpg',
+    ],
+    4 => [
+        'name' => 'Куртка для сноуборда DC Mutiny Charocal',
+        'category' => 'Одежда',
+        'price' => '7500',
+        'img' => 'img/lot-5.jpg',
+    ],
+    5 => [
+        'name' => 'Маска Oakley Canopy',
+        'category' => 'Разное',
+        'price' => '7500',
+        'img' => 'img/lot-6.jpg',
+    ],
+
+];
+
 
 ?>
 <!DOCTYPE html>
@@ -80,17 +120,18 @@ $category = ['Доски и лыжи', 'Крепления', 'Ботинки', '
             <h2>Открытые лоты</h2>
         </div>
         <ul class="lots__list">
+        <?php foreach ($goods as $item): ?>
             <li class="lots__item lot">
                 <div class="lot__image">
-                    <img src="img/lot-1.jpg" width="350" height="260" alt="Сноуборд">
+                    <img src="<?= $item['img'] ?>" width="350" height="260" alt="Сноуборд">
                 </div>
                 <div class="lot__info">
-                    <span class="lot__category">Доски и лыжи</span>
-                    <h3 class="lot__title"><a class="text-link" href="lot.html">2014 Rossignol District Snowboard</a></h3>
+                    <span class="lot__category"><?= $item['category'] ?></span>
+                    <h3 class="lot__title"><a class="text-link" href="lot.html"><?= $item['name'] ?></a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost">10 999<b class="rub">р</b></span>
+                            <span class="lot__cost"><?=number_format($item['price'], 0, '', '&nbsp;') . '<b class="rub">р</b></span>'; ?>
                         </div>
                         <div class="lot__timer timer">
 
@@ -98,6 +139,7 @@ $category = ['Доски и лыжи', 'Крепления', 'Ботинки', '
                     </div>
                 </div>
             </li>
+        <?php endforeach; ?>
         </ul>
     </section>
 </main>
